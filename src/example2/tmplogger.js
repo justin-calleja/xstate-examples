9e303a13f418b908037d27255f59773406d15dd5
Justin Calleja <calleja.justin@gmail.com>
2019-09-24 08:50:33 +0200

    saving

--- src/example2/tmplogger.js
+++ src/example2/tmplogger.js
@@ -1,50 +1,50 @@
-const { Machine, send, assign, spawn, interpret } = require("xstate");
+const { Machine, send, assign, spawn, interpret } = require('xstate');
 const {
   readConfigMachine,
   okTransitionCreator,
-  noFileTransitionCreator
-} = require("./readConfigMachine");
-const { createConfigMachine } = require("./createConfigMachine");
+  noFileTransitionCreator,
+} = require('./readConfigMachine');
+const { createConfigMachine } = require('./createConfigMachine');
 
 const loggerMachine = Machine(
   {
-    id: "logger",
-    initial: "standBy",
+    id: 'logger',
+    initial: 'standBy',
     context: {
-      verbose: false
+      verbose: false,
     },
     states: {
       standBy: {
         on: {
-          LOG: { internal: true, actions: "logMsg" }
-        }
-      }
-    }
+          LOG: { internal: true, actions: 'logMsg' },
+        },
+      },
+    },
   },
   {
     actions: {
       logMsg: (context, event) => {
-        console.log("in logMsg with context:", context, "event:", event);
-      }
-    }
-  }
+        console.log('in logMsg with context:', context, 'event:', event);
+      },
+    },
+  },
 );
 
 const testMachine = Machine({
-  id: "testMachine",
-  initial: "main",
+  id: 'testMachine',
+  initial: 'main',
   context: {
-    config: null
+    config: null,
   },
   states: {
     main: {
       entry: assign({
-        loggerRef: (context, event) => spawn(loggerMachine, loggerMachine.id)
+        loggerRef: (context, event) => spawn(loggerMachine, loggerMachine.id),
       }),
       invoke: {
         src: readConfigMachine,
-        onDone: [okTransitionCreator(), noFileTransitionCreator()]
-      }
+        onDone: [okTransitionCreator(), noFileTransitionCreator()],
+      },
     },
     ok: {
       //   actions: () => console.log("in testMachine.ok")
@@ -53,19 +53,19 @@ const testMachine = Machine({
     noFile: {
       //   entry: () => console.log("in testMachine.noFile entry"),
       //   entry: (context, event) => console.log("in testMachine.noFile entry, context:", context, event),
-      entry: send("LOG", {
-        to: context => context.loggerRef
+      entry: send('LOG', {
+        to: context => context.loggerRef,
         // to: "logger"
-      })
+      }),
       //   invoke: {
       //     src: createConfigMachine,
       //     // onDone: [okTransitionCreator(), noFileTransitionCreator()]
       //   }
-    }
-  }
+    },
+  },
 });
 
-interpret(testMachine)
-  //   .onTransition(state => {
-  //   })
-  .start();
+const testService = interpret(testMachine).start();
+
+// testService.sendTo('LOG', testService.state.context.loggerRef);
+// console.log('>>>>>>>>>>>', testService.state.context.loggerRef);
